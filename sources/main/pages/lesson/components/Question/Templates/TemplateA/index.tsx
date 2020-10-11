ab105d523f3b024932a98357255b76c439112bbb
Adriano Souza Costa <adriano.souza@consultaremedios.com.br>
2020-10-11 12:50:34 -0300

    Template A created

--- sources/main/pages/lesson/components/Question/Templates/TemplateA/index.tsx
+++ sources/main/pages/lesson/components/Question/Templates/TemplateA/index.tsx
@@ -7,7 +7,7 @@ import { Container } from "./styles";
 const TemplateA = () => {
   return (
     <Container>
-      <AnswerButton label="a)" title="45 horas" isCorrect={true} />
+      <AnswerButton label="a)" title="45 horas" />
       <AnswerButton label="b)" title="35 horas" />
       <AnswerButton label="c)" title="44 horas" selected={true} />
       <AnswerButton label="d)" title="24 horas" />
